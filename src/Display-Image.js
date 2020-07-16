2ae465c2c34420dfc953a3ec726c4d477a9932bf
Gregory Wilson <gjwilson7390@gmail.com>
2020-07-16 14:56:05 -0500

    Got MVP - styled stuff with styled-components

--- src/Display-Image.js
+++ src/Display-Image.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import "./App.css"
+import styled from 'styled-components'
+
+const Image = styled.img`
+    border-radius: 20px;
+    transition: 0.5s;
+    border: 3px solid black;
+
+    &:hover {
+    filter: brightness(170%);
+    transition: 0.5s;
+    }
+`;
 
 function DisplayImage (props) {
 
@@ -7,7 +19,7 @@ function DisplayImage (props) {
   
     return (
       <div className="photo-of-the-day">
-           <img src={photograph} alt='website logo' className="nasa-img" />
+           <Image src={photograph} alt='website logo' className="nasa-img" />
       </div>
     )
   
